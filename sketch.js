8528fd74f3c0d93216e782eabd5cbda184a9da11
Isabellegas <158753597+Isabellegas@users.noreply.github.com>
2024-02-17 11:16:37 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+let imgjogador, imgobstaculo, imgfundo;
+let obstaculo = [];
+let fundo = [];
+let jogador;
+
+function preload(){
+  imgjogador = loadImage("imagens/2.png");
+  imgobstaculo = loadImage("imagens/1.png");
+  imgfundo = loadImage("imagens/fundo.png");
+  
+}
+
+function setup() {
+  createCanvas(windowWidth/2, windowHeight/2);
+  jogador = new Jogador();
+}
+
+function draw() {
+  background(220);
+  jogador.show();
+}
